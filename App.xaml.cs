f356909f27e9939a4ce090f2f5adc26d3d7c495c
StetcoBianca <bstetco@yahoo.ro>
2024-11-29 22:12:12 +0100

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace StetcoBiancaLab7
+﻿
+using System;
+using StetcoBiancaLab7.Data;
+using System.IO;
+
+
+namespace StetcoBiancaLab7
 {
     public partial class App : Application
     {
+
+        static ShoppingListDatabase? database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
