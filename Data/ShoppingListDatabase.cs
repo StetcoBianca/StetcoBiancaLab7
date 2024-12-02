d6ee66067da81fa679ec03efdf39724d5a292622
StetcoBianca <bstetco@yahoo.ro>
2024-12-02 11:08:29 +0200

    Lab9

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -18,6 +18,8 @@ namespace StetcoBiancaLab7.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
         public Task<List<ShopList>> GetShopListsAsync()
         {
@@ -45,5 +47,47 @@ namespace StetcoBiancaLab7.Data
             return _database.DeleteAsync(slist);
         }
 
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
+        }
+
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
     }
 }
+
+
--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -21,4 +21,46 @@ public partial class ListPage : ContentPage
         await Navigation.PopAsync();
     }
 
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        var selectedProduct = listView.SelectedItem as Product;
+        if (selectedProduct != null)
+        {
+            bool confirmDelete = await DisplayAlert("Confirm Delete",
+                $"Are you sure you want to delete {selectedProduct.Description}?",
+                "Yes", "No");
+
+            if (confirmDelete)
+            {
+                await App.Database.DeleteProductAsync(selectedProduct);
+                listView.ItemsSource = await App.Database.GetListProductsAsync(((ShopList)BindingContext).ID);
+            }
+        }
+        else
+        {
+            await DisplayAlert("Error", "Please select an item to delete.", "OK");
+        }
+    }
+
 }
